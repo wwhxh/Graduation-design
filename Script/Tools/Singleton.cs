87ae3e72bced8b16e4675823a74bdfbe373df331
wwhxh <71277515+wwhxh@users.noreply.github.com>
2022-05-09 18:39:50 +0800

    Add files via upload

---
+++ Script/Tools/Singleton.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+    static T instance;
+    public static T Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = (T)this;
+        }
+    }
+
+    public static bool IsInitialized
+    {
+        get
+        {
+            return instance == null;
+        }
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if(instance == this)
+        {
+            instance = null;
+        }
+    }
+}
